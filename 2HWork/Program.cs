a1431d011bbf96a8e20f34acb62bbe740b8ca77d
Nozim Yokubov <anorcomander@gmail.com>
2024-03-30 21:23:32 +0500

    added 4seminar:2HWork

---
+++ 2HWork/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [344 452 341 125] => 2
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+
+    }
+    Console.Write("]");
+}
+
+
+int CountEvenNumInArray (int[] arr)
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            num++;
+        }
+    }
+    return num;
+}
+
+
+int size = 10;
+int[] arr = CreateArrayRndInt(size, 100, 1000);
+PrintArray(arr);
+
+int result = CountEvenNumInArray(arr);
+Console.WriteLine();
+Console.WriteLine(result);
